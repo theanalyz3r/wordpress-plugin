1a912b553d34db835c5d1728d405cf7d739380d3
Sergey Danilishin <sergey.danilishin@dev-pro.net>
2017-08-23 18:40:02 +0300

    Plugin version bump to 0.1.3

--- qordoba.php
+++ qordoba.php
@@ -3,7 +3,7 @@
 Plugin Name: Qordoba
 Plugin URI: http://qordoba.com/
 Description: Translate content of your multilingual website with Qordoba.
-Version: 0.1.0
+Version: 0.1.3
 Author: Qordoba
 Author URI: http://qordoba.com
 Text Domain: qordoba
@@ -16,7 +16,7 @@ if ( !function_exists('add_action') ) {
 
 define('QORDOBA_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('QORDOBA_PLUGIN_URL', plugin_dir_url(__FILE__));
-define('QORDOBA_VERSION', '0.1.0');
+define('QORDOBA_VERSION', '0.1.3');
 define('QORDOBA_API_URL', 'https://app.qordoba.com/api/');
 
 if (!defined('PLL_LINGOTEK_AD')) {
